d05188de398a3934f76daaa000a53457acc4fc68
ValeryArhipov <Valeryarhy@yandex.ru>
2025-03-23 19:06:59 +0300

    конец дз

---
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,23 @@
+package com.example.playlistmaker
+
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_settings)
+
+        val backArrow = findViewById<ImageView>(R.id.backArrow)
+
+        backArrow.setOnClickListener {
+            finish()
+        }
+    }
+}
