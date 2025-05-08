71c76525cf1008db2ceb5587fea3ce25eadfbdd2
ValeryArhipov <Valeryarhy@yandex.ru>
2025-05-09 01:06:21 +0300

    11 спринт

---
+++ app/src/main/java/com/example/playlistmaker/SearchResponse.kt
@@ -0,0 +1,7 @@
+package com.example.playlistmaker
+
+data class SearchResponse(
+    val resultCount: Int,
+    val results: List<Track>
+)
+
--- app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
+++ app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
@@ -1,17 +1,25 @@
 package com.example.playlistmaker
 
+import android.icu.text.SimpleDateFormat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import java.util.Locale
 
-class TrackAdapter(private val trackList: List<Track>) :
-    RecyclerView.Adapter<TrackAdapter.TrackViewHolder>() {
+class TrackAdapter : RecyclerView.Adapter<TrackAdapter.TrackViewHolder>() {
+
+    var trackList = ArrayList<Track>()
+
+    fun setTracks(tracks: List<Track>) {
+        trackList.clear()
+        trackList.addAll(tracks)
+        notifyDataSetChanged()
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
         val view = LayoutInflater.from(parent.context)
@@ -30,13 +38,14 @@ class TrackAdapter(private val trackList: List<Track>) :
     class TrackViewHolder(parentView: View) : RecyclerView.ViewHolder(parentView) {
         private val trackName: TextView = parentView.findViewById(R.id.trackName)
         private val artistName: TextView = parentView.findViewById(R.id.artistName)
-        private val trackTime: TextView = parentView.findViewById(R.id.trackTime)
+        private val trackTimeMillis: TextView = parentView.findViewById(R.id.trackTime)
         private val artworkUrl100: ImageView = parentView.findViewById(R.id.icon_album)
 
         fun bind(track: Track) {
             trackName.text = track.trackName
             artistName.text = track.artistName
-            trackTime.text = track.trackTime
+            trackTimeMillis.text = formatTime(track.trackTimeMillis)
+
 
             Glide.with(itemView.context)
                 .load(track.artworkUrl100)
@@ -44,6 +53,11 @@ class TrackAdapter(private val trackList: List<Track>) :
                 .transform(RoundedCorners(4))
                 .into(artworkUrl100)
         }
+        private fun formatTime(millis: Long): String {
+            val dateFormat = SimpleDateFormat("mm:ss", Locale.getDefault())
+            return dateFormat.format(millis)
+        }
+
     }
 }
 
